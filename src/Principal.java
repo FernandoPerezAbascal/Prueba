579946f3022e69669a9b8c2bca8b8291086b2bd7
Fernando <Fernando@S-1.Home>
2018-05-24 17:44:48 +0200

    cambiado una masyucula

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method 
 		
-		System.out.println("HOla mundo");
+		System.out.println("Hola mundo");
 
 	}
 
